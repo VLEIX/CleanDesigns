5968449299111a7a9891e1d2bed843f0a7c103e4
Flavio Franco Tunqui <flavio.franco.tunqui@fisglobal.com>
2019-06-03 17:54:10 -0500

    minimal finished

--- lib/minimal.dart
+++ lib/minimal.dart
@@ -21,13 +21,17 @@ class _MinimalState extends State<Minimal> {
         centerTitle: true,
         leading: IconButton(
           icon: Icon(Icons.arrow_back),
+          tooltip: 'Back',
           color: Colors.black,
           iconSize: 17.0,
-          onPressed: () {},
+          onPressed: () {
+            Navigator.pop(context);
+          },
         ),
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.search),
+            tooltip: 'Search',
             color: Colors.black,
             iconSize: 17.0,
             onPressed: () {},
@@ -106,7 +110,103 @@ class _MinimalState extends State<Minimal> {
                 ),
               ],
             ),
-          )
+          ),
+          SizedBox(
+            height: 15.0,
+          ),
+          firstStyleRow(
+            'images/cone.jpeg',
+            'images/letter.jpeg',
+            'images/chris.jpg',
+            'Chris Pratt',
+            '10:51 PM',
+            'I like the way to place Items to show more information',
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          secondStyleRow(
+            'images/window.jpeg',
+            'images/cactus.jpeg',
+            'images/tomcruise.jpg',
+            'Tom Cruise',
+            '10:40 PM',
+            'I like the way to place Items to show more information',
+          ),
+          thirdStyleRow(
+            'images/hugh.jpg',
+            'Hugh Jackman',
+            '9:30 PM',
+            'I like the way to place Items to show more information',
+            'images/johnnydepp.jpg',
+            'Johnny Depp',
+            '9:24 PM',
+            'I like the way to place Items to show more information',
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          firstStyleRow(
+            'images/cone.jpeg',
+            'images/letter.jpeg',
+            'images/chris.jpg',
+            'Chris Pratt',
+            '10:51 PM',
+            'I like the way to place Items to show more information',
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          secondStyleRow(
+            'images/window.jpeg',
+            'images/cactus.jpeg',
+            'images/tomcruise.jpg',
+            'Tom Cruise',
+            '10:40 PM',
+            'I like the way to place Items to show more information',
+          ),
+          thirdStyleRow(
+            'images/hugh.jpg',
+            'Hugh Jackman',
+            '9:30 PM',
+            'I like the way to place Items to show more information',
+            'images/johnnydepp.jpg',
+            'Johnny Depp',
+            '9:24 PM',
+            'I like the way to place Items to show more information',
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          firstStyleRow(
+            'images/cone.jpeg',
+            'images/letter.jpeg',
+            'images/chris.jpg',
+            'Chris Pratt',
+            '10:51 PM',
+            'I like the way to place Items to show more information',
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          secondStyleRow(
+            'images/window.jpeg',
+            'images/cactus.jpeg',
+            'images/tomcruise.jpg',
+            'Tom Cruise',
+            '10:40 PM',
+            'I like the way to place Items to show more information',
+          ),
+          thirdStyleRow(
+            'images/hugh.jpg',
+            'Hugh Jackman',
+            '9:30 PM',
+            'I like the way to place Items to show more information',
+            'images/johnnydepp.jpg',
+            'Johnny Depp',
+            '9:24 PM',
+            'I like the way to place Items to show more information',
+          ),
         ],
       ),
     );
@@ -155,6 +255,207 @@ class _MinimalState extends State<Minimal> {
           ],
         )
       ],
+    );
+  }
+
+  Widget infoUser(
+      String imgAvatar, String name, String hour, String description) {
+    return Container(
+      height: 100.0,
+      padding: EdgeInsets.all(5.0),
+      child: Column(
+        children: <Widget>[
+          Text(
+            description,
+            overflow: TextOverflow.ellipsis,
+            textAlign: TextAlign.left,
+            maxLines: 2,
+            style: TextStyle(
+              fontFamily: 'Montserrat',
+            ),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Row(
+            children: <Widget>[
+              Container(
+                height: 30.0,
+                width: 30.0,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15.0),
+                    image: DecorationImage(
+                      image: AssetImage(imgAvatar),
+                      fit: BoxFit.cover,
+                    )),
+              ),
+              SizedBox(
+                width: 10.0,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    name,
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.orange,
+                    ),
+                  ),
+                  Text(
+                    hour,
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      fontFamily: 'Montserrat',
+                      color: Colors.grey,
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget imageRounded(String path, double height) {
+    return Container(
+      height: height,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        image: DecorationImage(
+          image: AssetImage(path),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+
+  Widget firstStyleRow(String imgPath1, String imgPath2, String imgAvatar,
+      String name, String hour, String description) {
+    return Container(
+      padding: EdgeInsets.only(
+        left: 10.0,
+        right: 10.0,
+      ),
+      child: Row(
+        children: <Widget>[
+          Flexible(
+            flex: 1,
+            child: Container(
+              child: Column(
+                children: <Widget>[
+                  imageRounded(
+                    imgPath1,
+                    160.0,
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  infoUser(
+                    imgAvatar,
+                    name,
+                    hour,
+                    description,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            width: 10.0,
+          ),
+          Flexible(
+            flex: 1,
+            child: imageRounded(
+              imgPath2,
+              270.0,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget secondStyleRow(String imgPath1, String imgPath2, String imgAvatar,
+      String name, String hour, String description) {
+    return Container(
+      padding: EdgeInsets.only(
+        left: 10.0,
+        right: 10.0,
+      ),
+      child: Row(
+        children: <Widget>[
+          Flexible(
+            flex: 1,
+            child: imageRounded(
+              imgPath2,
+              270.0,
+            ),
+          ),
+          SizedBox(
+            width: 10.0,
+          ),
+          Flexible(
+            flex: 1,
+            child: Container(
+              child: Column(
+                children: <Widget>[
+                  infoUser(
+                    imgAvatar,
+                    name,
+                    hour,
+                    description,
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  imageRounded(
+                    imgPath1,
+                    160.0,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget thirdStyleRow(
+      String imgAvatar1,
+      String name1,
+      String hour1,
+      String description1,
+      String imgAvatar2,
+      String name2,
+      String hour2,
+      String description2) {
+    return Container(
+      padding: EdgeInsets.only(
+        left: 10.0,
+        right: 10.0,
+      ),
+      child: Row(
+        children: <Widget>[
+          Flexible(
+            flex: 1,
+            child: infoUser(imgAvatar1, name1, hour1, description1),
+          ),
+          SizedBox(
+            width: 10.0,
+          ),
+          Flexible(
+            flex: 1,
+            child: infoUser(imgAvatar2, name2, hour2, description2),
+          ),
+        ],
+      ),
     );
   }
 }
