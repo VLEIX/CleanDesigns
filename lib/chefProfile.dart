3ac4740947b4d237ba27e88b07333dbc97d86695
Flavio Franco <vleix.92@gmail.com>
2019-09-16 23:58:05 -0500

    appbar transparent implemented

--- lib/chefProfile.dart
+++ lib/chefProfile.dart
@@ -9,32 +9,100 @@ class _ChefProfileState extends State<ChefProfile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFFFC634B),
       body: Stack(
         children: <Widget>[
-          Positioned(
-            height: 44.0,
+          Opacity(
+            opacity: 0.2,
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('images/food_pattern.png'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+          ListView(
+            children: <Widget>[
+              Container(
+                height: 100.0,
+                width: double.infinity,
+                color: Colors.transparent,
+              ),
+              Container(
+                height: 100.0,
+                width: double.infinity,
+                color: Colors.black,
+              ),
+              Container(
+                height: 100.0,
+                width: double.infinity,
+                color: Colors.white,
+              ),
+              Container(
+                height: 100.0,
+                width: double.infinity,
+                color: Colors.black,
+              ),
+              Container(
+                height: 100.0,
+                width: double.infinity,
+                color: Colors.white,
+              ),
+              Container(
+                height: 100.0,
+                width: double.infinity,
+                color: Colors.black,
+              ),
+              Container(
+                height: 100.0,
+                width: double.infinity,
+                color: Colors.white,
+              ),
+              Container(
+                height: 100.0,
+                width: double.infinity,
+                color: Colors.black,
+              ),
+              Container(
+                height: 100.0,
+                width: double.infinity,
+                color: Colors.white,
+              ),
+              Container(
+                height: 100.0,
+                width: double.infinity,
+                color: Colors.black,
+              ),
+              Container(
+                height: 100.0,
+                width: double.infinity,
+                color: Colors.white,
+              ),
+              Container(
+                height: 100.0,
+                width: double.infinity,
+                color: Colors.green,
+              ),
+            ],
+          ),
+          Container(
+            height: 80.0,
+            width: double.infinity,
             child: AppBar(
-              title: Text('dsd'),
-              backgroundColor: Colors.red,
+              backgroundColor: Colors.transparent,
               elevation: 0.0,
               leading: IconButton(
                 icon: Icon(Icons.arrow_back_ios),
                 tooltip: 'Back',
-                color: Colors.black,
+                color: Colors.white,
                 onPressed: () {
                   Navigator.pop(context);
                 },
               ),
             ),
-          ),
-          Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('images/food_pattern.png'),
-                fit: BoxFit.fill,
-              ),
-            ),
-          ),
+          )
         ],
       ),
     );
